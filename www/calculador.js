28e1690e977ea3c50f4fe9e50faed4ad06899b6d
AnaTeresaQR <ana.quesada@ucrso.info>
2015-12-07 15:14:56 -0600

    cambio de márgenes de resultados

--- www/calculador.js
+++ www/calculador.js
@@ -48,31 +48,31 @@ function calcularIMC(peso, talla) {
  */
 function clasificar(imc) {
 
-  if (imc <= 18.5) {
+  if (imc < 18.5) {
     return "Bajo Peso";
   }
 
-  if (imc > 18.5 && imc <= 24.9) {
+  if (imc >= 18.5 && imc <= 24.9) {
     return "Normal";
   }
 
-  if (imc > 24.9 && imc <= 29.9) {
+  if (imc >= 25 && imc <= 29.9) {
     return "Sobrepeso";
   }
 
-  if (imc > 29.9 && imc <= 34.9) {
+  if (imc >= 30 && imc <= 34.9) {
     return "Obesidad I";
   }
 
-  if (imc > 34.9 && imc <= 39.9) {
+  if (imc >= 35 && imc <= 39.9) {
     return "Obesidad II";
   }
 
-  if (imc > 39.9 && imc <= 49.9) {
+  if (imc >= 40 && imc <= 49.9) {
     return "Obesidad Mórbida";
   }
 
-  if (imc > 49.9) {
+  if (imc > 50) {
     return "Súper Obesidad";
   }
 }
